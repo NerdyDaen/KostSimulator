e7312e6d87fbb3857a8b1cb7f8e40c1310e0f257
Nerdy_Daen <Sonia.prameswari@students.amikom.ac.id>
2022-06-21 07:56:23 +0700

    Camera Recentre
    - Camera will re-center when the player holds the left ctrl key (it was auto re-center)

---
+++ KostSimulator_Project_2020.3.25F1/Assets/Scripts/Events/CameraRecentre.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraRecentre : MonoBehaviour
+{
+    private CinemachineFreeLook camera;
+
+    private void Start()
+    {
+        camera = GetComponent<CinemachineFreeLook>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("CameraRecentre") == 1)
+        {
+            camera.m_RecenterToTargetHeading.m_enabled = true;
+        }
+        else
+        {
+            camera.m_RecenterToTargetHeading.m_enabled = false;
+        }
+    }
+}
